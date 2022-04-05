ad6c6fde18b7078d051e8a28d5e9ce334ce6ade1
Saltykov21ist2 <99250746+Saltykov21ist2@users.noreply.github.com>
2022-04-05 11:46:40 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,9 +1,9 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
-
+//nmnmn
 template<typename T, int size>
 class TStack {
-
+//fgfg
 };
 #endif  // INCLUDE_TSTACK_H_
